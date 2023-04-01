0f41942402b44261d734d863bb91635bb1ed1f36
Michal Posluszny <mposluszny.foto@gmail.com>
2023-04-01 17:20:16 +0200

    chuj dziala

---
+++ godot/dragon.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const ACCELERATION = 10
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	move(delta)
+
+
+func move(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	input_vector = input_vector.normalized()
+	velocity = input_vector * SPEED
+	velocity = velocity.move_toward(input_vector * SPEED, ACCELERATION * delta)
+	move_and_slide()
