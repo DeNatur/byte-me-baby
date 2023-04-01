82506ec49272ac9604f7da6a29271e5bf2fe61f9
Michal Posluszny <mposluszny.foto@gmail.com>
2023-04-01 21:35:29 +0200

    kurwa akcje plejera

--- godot/player/player.gd
+++ godot/player/player.gd
@@ -6,6 +6,7 @@ const SPEED = 300.0
 const ACCELERATION = 10
 
 signal skill_used(skill_type, node_name)
+signal balance_change(current_balance)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,11 +15,14 @@ var animation_player = $AnimationPlayer
 @onready
 var sprite_player = $Sprite2D
 var direction = MoveDirection.RIGHT
+var current_skill = "Fly"
+var current_view = Vector2.ZERO
+var in_action = false
 
-func _physics_process(delta):
-	if not $Timer.is_stopped():
-		await $Timer.timeout
-	if Input.is_action_just_pressed("interact"):	
+func _process(delta):
+	if in_action:
+		pass
+	elif Input.is_action_just_pressed("interact"):	
 		skill(delta)
 	else:
 		move(delta)
@@ -36,6 +40,7 @@ func move(delta):
 	move_and_slide()
 	var areaPos = velocity.normalized()
 	if (areaPos != Vector2.ZERO):
+		current_view = Vector2(areaPos.x*64,areaPos.y*100)
 		$interactiveArea.position = Vector2(areaPos.x*64,areaPos.y*100)
 
 func set_animation(input_vector):
@@ -55,6 +60,25 @@ func set_idle_anim():
 	sprite_player.set_flip_h(direction == MoveDirection.LEFT)
 			
 func skill(delta):
-	print("Cry")
-	animation_player.play("Cry")
-	$Timer.start()
+	in_action = true
+	$AnimStart.start()
+	animation_player.play(current_skill)
+	use_skill()
+
+	
+func use_skill():
+	
+	if current_skill == "Cry":
+		await $AnimStart.timeout
+	elif current_skill == "Fly":
+		await $AnimStart.timeout
+		$AnimEnd.start()
+		var view_norm = current_view.normalized()
+		var col_size =  $collision.shape.size + Vector2(100,100)
+		position = position + Vector2(view_norm.x*col_size.x,view_norm.y*col_size.y)
+		move_and_slide()
+
+	in_action = false
+
+
+	
