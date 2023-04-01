ffc6ce4b02e98af405c0d5d45b212ea0c03159fc
Michal Posluszny <mposluszny.foto@gmail.com>
2023-04-01 17:39:12 +0200

    added signal

--- godot/player/player.gd
+++ godot/player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const ACCELERATION = 10
 const JUMP_VELOCITY = -400.0
+signal skill_used(skill_type)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -11,6 +12,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	move(delta)
+	skill(delta)
 
 
 func move(delta):
@@ -21,3 +23,6 @@ func move(delta):
 	velocity = input_vector * SPEED
 	velocity = velocity.move_toward(input_vector * SPEED, ACCELERATION * delta)
 	move_and_slide()
+
+func skill(delta):
+	pass
