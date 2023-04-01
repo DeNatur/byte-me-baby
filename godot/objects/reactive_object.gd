2ef7c8d93008f85fa80c059bb3dbb8846fbc5d7d
Konrad <konrad@MacBook-Pro-Konrad.local>
2023-04-01 21:47:52 +0200

    Add highlighting object of choose

--- godot/objects/reactive_object.gd
+++ godot/objects/reactive_object.gd
@@ -2,11 +2,13 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Active.hide()
 	var player = get_parent().get_node("player")
 	var area2d = get_node("Area2D")
 
 	player.connect("skill_used", _on_player_skill_used)
 	area2d.connect("area_entered", _on_area_entered)
+	area2d.connect("area_exited", _on_area_exited)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,5 +19,9 @@ func _on_player_skill_used(skill_type):
 	print("skilled detected")
 	
 func _on_area_entered(body):
+	$Active.show()
 	print("area entered: ", body.get_parent().name)
 	
+func _on_area_exited(body):
+	$Active.hide()
+	print("area exited")
