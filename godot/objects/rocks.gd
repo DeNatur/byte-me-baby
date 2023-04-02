0583fa8d2d581e2a490a95ac3e944eea856e201a
Konrad Startek <52463672+evo-brut3@users.noreply.github.com>
2023-04-02 05:16:41 +0200

    Add new objects for skills (#6)
    Co-authored-by: Konrad <konrad@MacBook-Pro-Konrad.local>

--- godot/objects/rocks.gd
+++ godot/objects/rocks.gd
@@ -1,32 +1,35 @@
 extends Node
 
-var required_skill = "Cry"
+var required_skill = "Smash"
 
-enum state {BURNING, NOT_BURNING}
+enum state {OFF, ON}
 
 @onready
-var sprite_bridge = $BurningBridge
+var sprites_anim = $AnimatedSprite2D
 
-var current_state = state.BURNING
+var current_state = state.ON
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.connect("timeout", _on_timer_timeout)
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
 func do_action():
-	current_state = state.NOT_BURNING
-	sprite_bridge.play("NotBurning")
-	required_skill = "NoBridgeNoCry"
+	current_state = state.OFF
+	#sprites_anim.play("Burning")
+	# Need to add waiting for animation to finish before the object is gone
+	required_skill = "NoRocksNoSmash"
 	get_parent().get_node("CollisionShape2D").call_deferred("set", "disabled", true)
+	$Sprite2D.hide()
 	$Timer.start()
 	
 func _on_timer_timeout():
-	current_state = state.BURNING
-	sprite_bridge.play("Burning")
-	required_skill = "Cry"
+	current_state = state.ON
+	required_skill = "Smash"
+	$Sprite2D.show()
 	get_parent().get_node("CollisionShape2D").call_deferred("set", "disabled", false)
 	
 
